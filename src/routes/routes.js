76ce320f63ac97de10480629e61e2bb3355ce540
guilbd@hotmail.com <guilbd@hotmail.com>
2021-09-08 23:01:25 -0300

    format database; routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,25 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
 const PersonagemController = require("../controllers/PersonagensController");
 const PersonagemMiddleware = require("../middlewares/PersonagemMiddlewares");
 
 routes.get("/personagens", PersonagemController.getAll);
-routes.get("/personagens/:id", PersonagemMiddleware.validaId, PersonagemController.getById);
-routes.post("/personagens", PersonagemController.create)
-routes.put("/personagens", PersonagemMiddleware.validaId, PersonagemController.update)
-routes.delete('/personagens', PersonagemMiddleware.validaId, PersonagemController.del)
+routes.get(
+  "/personagens/:id",
+  PersonagemMiddleware.validaId,
+  PersonagemController.getById
+);
+routes.post("/personagens", PersonagemController.create);
+routes.put(
+  "/personagens",
+  PersonagemMiddleware.validaId,
+  PersonagemController.update
+);
+routes.delete(
+  "/personagens",
+  PersonagemMiddleware.validaId,
+  PersonagemController.del
+);
 
-
-module.exports = routes
+module.exports = routes;
