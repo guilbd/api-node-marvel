0c1e848ba0ef317dbe2140cb859fdbd7a4ca63ec
guilbd@hotmail.com <guilbd@hotmail.com>
2021-09-08 22:43:35 -0300

    update/delete - controller, middleware e routes

--- src/controllers/PersonagensController.js
+++ src/controllers/PersonagensController.js
@@ -1,54 +1,92 @@
-const Personagem = require('../models/Personagem')
+const Personagem = require("../models/Personagem");
 
 const getAll = async (req, res) => {
-    try{
-        const personagens = await Personagem.find() //find é uma Promise
-        return res.send({personagens})
-    } catch(err){
-        res.status(500).send({error: err})
-    }
+  try {
+    const personagens = await Personagem.find(); //find é uma Promise
+    return res.send({ personagens });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
 };
 
 const getById = async (req, res) => {
-    const {id} = req.params.id;
+  const { id } = req.params.id;
 
-    try{
-        const personagem = await Personagem.findById(id);
-        if(!personagem){
-            res.status(404).send({message: "personagem não encontrado"})
-            return;
-        };
-        return res.status({personagem})
-    } catch(err){
-        res.status(500).send({error: err});
-    };
-}
+  try {
+    const personagem = await Personagem.findById(id);
+    if (!personagem) {
+      res.status(404).send({ message: "personagem não encontrado" });
+      return;
+    }
+    return res.status({ personagem });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+};
 
 const create = async (req, res) => {
-    const {nome, identidade, genero, imagem} = req.body;
+  const { nome, identidade, genero, imagem } = req.body;
 
-    if(!nome || !identidade || !genero || !imagem){
-        res.status(404).send({message: "Você não enviou todos os dados necessários para o cadastro"});
-        return;
-    }
-
-    const novoPersonagem = await Personagem({
-        nome,
-        identidade,
-        genero, 
-        imagem
+  if (!nome || !identidade || !genero || !imagem) {
+    res.status(404).send({
+      message: "Você não enviou todos os dados necessários para o cadastro",
     });
+    return;
+  }
 
-    try {
-        await novoPersonagem.save();
-        return res.status(201).send({message: "Personagem criado com sucesso", novoPersonagem});
-    } catch(err){
-        res.status(500).send({error: err})
-    }
-}
+  const novoPersonagem = await Personagem({
+    nome,
+    identidade,
+    genero,
+    imagem,
+  });
+
+  try {
+    await novoPersonagem.save();
+    return res
+      .status(201)
+      .send({ message: "Personagem criado com sucesso", novoPersonagem });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+};
+
+const update = async (req, res) => {
+  const { nome, identidade, genero, imagem } = req.body;
+
+  if (!nome || !identidade || !genero || !imagem) {
+    res.status(400).send({
+      message: "Você não enviou todos os dados necessários para o cadastro",
+    });
+    return;
+  }
+
+  res.personagem.nome = nome;
+  res.personagem.identidade = identidade;
+  res.personagem.genero = genero;
+  res.personagem.imagem = imagem;
+
+  try {
+    await res.personagem.save();
+    res.send({ message: "Personagem alterado com sucesso!" });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+};
+
+const del = async (req, res) => {
+  try {
+    await res.personagem.remove();
+    res.send({ message: "personagem removido com sucesso" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
 module.exports = {
-    getAll,
-    getById,
-    create
-}
+  getAll,
+  getById,
+  create,
+  update,
+  del,
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,8 @@ const PersonagemMiddleware = require("../middlewares/PersonagemMiddlewares");
 routes.get("/personagens", PersonagemController.getAll);
 routes.get("/personagens/:id", PersonagemMiddleware.validaId, PersonagemController.getById);
 routes.post("/personagens", PersonagemController.create)
+routes.put("/personagens", PersonagemMiddleware.validaId, PersonagemController.update)
+routes.delete('/personagens', PersonagemMiddleware.validaId, PersonagemController.del)
 
 
 module.exports = routes
